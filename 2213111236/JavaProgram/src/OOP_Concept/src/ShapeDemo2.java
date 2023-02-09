868feae510c30c00e7203ae07bd594cb9acee7af
ArisuKusu <tatenashi003@gmail.com>
2023-02-09 11:56:37 +0700

    9-2-2023

--- 2213111236/JavaProgram/src/OOP_Concept/src/ShapeDemo2.java
+++ 2213111236/JavaProgram/src/OOP_Concept/src/ShapeDemo2.java
@@ -1,8 +1,23 @@
-
+import java.util.Scanner;
 public class ShapeDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Cylinder[] cylinder = new Cylinder[5];
+		for(int i = 0;i<cylinder.length;i++) {
+			System.out.print("");
+			double radius = scan.nextDouble();
+			System.out.print("");
+			double height = scan.nextDouble();
+			cylinder[i] = new Cylinder(radius,height);
+			
+		}
+		int a = 1;
+		for(Cylinder i : cylinder) {
+			System.out.print("Cylinder "+a+", Vlume="+i.getVolume());
+			a++;
+		}
 
 	}
 
